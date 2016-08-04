625933e5ce9492a950d04a02957d62333b174710
HeesuKim <moony416@naver.com>
2016-08-04 15:34:46 +0900

    게임 시작 플래그

--- app/src/main/java/test/testapp/MainActivity.java
+++ app/src/main/java/test/testapp/MainActivity.java
@@ -1,15 +1,21 @@
 package test.testapp;
 
+import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(new GameView(this));
         /*Display dis = getWindowManager().getDefaultDisplay();
         System.out.println("wid : " + dis.getWidth());
